e30a3e85360f58428e753b86b3c4fc601aa4f110
Artur Ostrovskiy <artur_ostrovskiy@mail.ru>
2025-11-08 17:30:20 +0300

    commit

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -11,6 +11,6 @@ return false;
 
 // console.log(isNumeric('123')) // Ожидаемый результат: true
 // console.log(isNumeric("12.3")) // Ожидаемый результат: true
-console.log(isNumeric("123abc")) // Ожидаемый результат: false
+// console.log(isNumeric("123abc")) // Ожидаемый результат: false
 // console.log(isNumeric("abc")) // Ожидаемый результат: false
 // console.log(isNumeric(" ")) // Ожидаемый результат: false
